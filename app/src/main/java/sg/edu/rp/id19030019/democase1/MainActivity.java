fdfe93ee10aa76a0c236b34dd4a487d99a04b1a6
anisaaffendi <19030019@rp.edu.sg>
2021-08-03 11:42:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/id19030019/democase1/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.id19030019.democase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
