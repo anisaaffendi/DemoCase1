2eddfd754c08341632f91c3a60aa71d17dd2626a
anisaaffendi <19030019@rp.edu.sg>
2021-08-03 11:51:25 +0800

    testest

--- app/src/main/java/sg/edu/rp/id19030019/democase1/MainActivity.java
+++ app/src/main/java/sg/edu/rp/id19030019/democase1/MainActivity.java
@@ -1,14 +1,71 @@
 package sg.edu.rp.id19030019.democase1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int requestCode = 123;
+    int notificationID = 888;
+    Button btnNotify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btnNotify);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+                bigTextStyle.setBigContentTitle("Deals from top electronic retailers");
+                bigTextStyle.bigText("Mobile 50% off" + "\n Laptop 20% off" + "\n Tablet 22% off" + "\n Printer 30% off" + "\n Other 10% off");
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Deals from top electronic retailers");
+                builder.setContentText("Excellent deals");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pendingIntent);
+                builder.setStyle(bigTextStyle);
+                builder.setAutoCancel(true);
+
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                builder.setSound(uri);
+
+                builder.setPriority(Notification.PRIORITY_HIGH);
+
+                Notification n = builder.build();
+
+                //an integer good to have, for you to programmatically cancel it
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
     }
 }
